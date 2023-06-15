ec165c5e87a885d0618b88ca95277f030757c19b
M1shanay <mishaborisov0159@gmail.com>
2023-06-15 12:29:35 +0300

    Tutorial+Size

--- Assets/Scripts/UI/DeathScreen.cs
+++ Assets/Scripts/UI/DeathScreen.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class DeathScreen : MonoBehaviour
 {
     public void ExitToMenu()
     {
-        YandexSDK.YaSDK.instance.ShowInterstitial();
+        //YandexSDK.YaSDK.instance.ShowInterstitial();
         SceneManager.LoadScene("Menu");
     }
     public void Retry()
     {
-        YandexSDK.YaSDK.instance.ShowInterstitial();
+        //YandexSDK.YaSDK.instance.ShowInterstitial();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
